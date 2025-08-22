3e7dace801741445874f36ef7bfb436672682564
YADAVALLI JAYANTH KUMAR <115489547+Jayanthkumar04@users.noreply.github.com>
2025-08-22 11:21:39 +0530

    Update CustomerService.java

--- kafka/ProducerService/src/main/java/com/example/producer/service/CustomerService.java
+++ kafka/ProducerService/src/main/java/com/example/producer/service/CustomerService.java
@@ -1,5 +1,27 @@
 package com.example.producer.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import com.example.producer.model.Customer;
+
+@Service
 public class CustomerService {
 
+	@Value("${topic.name}")
+	String topicName;
+	@Autowired
+	KafkaTemplate<String, Customer> kafkaTemplate;
+	
+	
+	public String addCustomer(Customer customer)
+	{
+	      kafkaTemplate.send(topicName,"spring", customer);
+	      
+	      return "customer has been added to kafka topic successfully";
+	}
+	
+	
 }
